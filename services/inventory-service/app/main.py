1a75d9681bbf5a3b15aa3c642b3bc462f5adf6a3
hodaoty <hodaoty69@gmail.com>
2025-11-03 22:18:45 +0700

    add inventory-service port 8003

---
+++ services/inventory-service/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from app.api.v1 import inventory
+from app.db.database import engine, Base
+
+# Yêu cầu SQLAlchemy tạo bảng "inventory" khi khởi động
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(inventory.router, prefix="/api", tags=["inventory"])
+
+@app.get("/")
+def read_root():
+    """Endpoint Healthcheck"""
+    return {"service": "Inventory Service is running"}
+
