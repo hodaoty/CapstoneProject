adebc584cdd2c39ea63bbd241bb66993ce7a1ddb
hodaoty <hodaoty69@gmail.com>
2025-11-04 09:33:12 +0700

    them cart-service: gio hang

---
+++ services/cart-service/app/core/config.py
@@ -0,0 +1,6 @@
+import os 
+
+class Settings():
+    REDIS_URL:str=os.environ.get("REDIS_URL","redis://localhost:6379")
+
+settings = Settings()
---
+++ services/cart-service/app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from app.api.v1 import cart
+
+app = FastAPI(title="Cart Service")
+
+app.include_router(cart.router, prefix="/api", tags=["cart"])
+
+@app.get("/")
+def read_root():
+    """Endpoint Healthcheck"""
+    return {"service": "Cart Service is running"}
